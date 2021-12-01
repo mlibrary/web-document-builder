77343a29aa93a328c1afbad653cf34103b236fe8
Jon Earley <me@yelrae.com>
2021-12-01 12:14:16 -0500

    Add colors!

--- block-serializer.js
+++ block-serializer.js
@@ -3,6 +3,6 @@ module.exports = {
     callout: require('./blocks/callout'),
     code_preview: require('./blocks/code-preview'),
     dont_and_do: require('./blocks/dont-and-do'),
-    inline_local_navigation: require('./blocks/inline-local-navigation')
+    colors: require('./blocks/colors')
   }
 }
---
+++ blocks/colors.js
@@ -0,0 +1,35 @@
+const tokens = require('../design-tokens')
+const h = require('hyperscript')
+
+const paletteTokens = tokens.props.reduce((acc, prop) => {
+  if (prop.meta && prop.meta.palette && !acc.includes(prop.meta.palette)) {
+    acc.push(prop.meta.palette)
+  }
+
+  return acc
+}, [])
+
+const palettes = paletteTokens.map((palette) => {
+  const shades = tokens.props.filter(prop => {
+    if (prop.meta && prop.meta.palette === palette) {
+      return true
+    }
+
+    return false
+  })
+
+  return shades
+}, [])
+
+module.exports = function Colors() {
+ return h('div.color-palettes',
+    palettes.map(palette => {
+      return h('ol.color-palette',
+        palette.map(color => h('li', {
+          id: color.name,
+          style: `background: var(--${color.name})`
+        }, color.name))
+      )
+    }
+  ))
+}
